f73a85df4aae19ac5facd9ac2fbd52c06ee6b26c
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2023-01-22 16:50:38 +0300

    add

--- config/app.php
+++ config/app.php
@@ -15,6 +15,8 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
+    'default' => env('DB_CONNECTION', 'pgsql'),
+
     /*
     |--------------------------------------------------------------------------
     | Application Environment
